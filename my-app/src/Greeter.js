ed79ad1cf0a5fbd1169e393de79ff94d28a0c903
HussainSaleh <hussainalazzani@hotmail.com>
2020-11-27 18:40:36 +0000

    Completed events

--- my-app/src/Greeter.js
+++ my-app/src/Greeter.js
@@ -1,10 +1,15 @@
 import React from "react";
 
 const Greeter = ({ name, age, health = 10 }) => {
+    const greet = () => {
+        alert(`Hello ${name}`);
+    };
     return (
         <div>
             <h1>Hello {name}</h1>
             <p>You are {age} and you health is {health}</p>
+            {/* This is a react onClick. Notice the Capital C */}
+            <button onClick={greet}>Click Me</button>
         </div>
     );
 };
